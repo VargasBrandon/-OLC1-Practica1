b44ca34d27439df685992ce4e60085e7a53d38ae
vargas <vargasbrandon805@gmail.com>
2020-02-07 22:35:11 -0600

    Version 1

---
+++ src/olc1/practica1/OLC1Practica1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc1.practica1;
+
+/**
+ *
+ * @author HP
+ */
+public class OLC1Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principal().setVisible(true);
+            }
+        });
+    }
+    
+}
